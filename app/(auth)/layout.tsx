919cc9897fd7f1f4200df47326d2224a616d4440
Khushboo Daryani <khushboodaryani1@gmail.com>
2025-07-05 13:45:49 +0530

    updated

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,33 @@
+import { ClerkProvider } from "@clerk/nextjs"
+import { Inter } from "next/font/google"
+import '../globals.css'
+import { Metadata } from "next";
+import { dark } from "@clerk/themes";
+
+const inter = Inter({ subsets: ["latin"] })
+export const metadata: Metadata = {
+    title: "Threads",
+    description: "Threads application",
+};
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <ClerkProvider
+            appearance={{
+                baseTheme: dark,
+            }}
+        >
+            <html lang="en">
+                <body className={`${inter.className} bg-dark-1`}>
+                    <div className="w-full flex justify-center items-center min-h-screen">
+                        {children}
+                    </div>
+                </body>
+            </html>
+        </ClerkProvider>
+    )
+}
---
+++ lib/uploadthing.ts
@@ -0,0 +1,7 @@
+// Refer uploadthing doc
+import { generateReactHelpers } from "@uploadthing/react";
+
+import type { OurFileRouter } from "@/app/api/uploadthing/core";
+
+export const { useUploadThing, uploadFiles } =
+  generateReactHelpers<OurFileRouter>();
